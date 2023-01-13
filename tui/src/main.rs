618c8fab4eeccaf5a406e061e06404b89b070e29
Kristoffer Andersson <kristoffer.andersson@gu.se>
2023-01-13 15:47:28 +0100

    refactor: raise log level for mio

--- tui/src/main.rs
+++ tui/src/main.rs
@@ -5,7 +5,7 @@ use env_logger::{Env, Target};
 fn main() {
     let log_file = fs::File::create("ancestors.log").expect("creating log file");
     let log_writer = Box::new(io::BufWriter::new(log_file));
-    env_logger::Builder::from_env(Env::default().default_filter_or("trace,rustyline=warn"))
+    env_logger::Builder::from_env(Env::default().default_filter_or("trace,rustyline=warn,mio=error"))
         .format_timestamp(None)
         .target(Target::Pipe(log_writer))
         .init();
