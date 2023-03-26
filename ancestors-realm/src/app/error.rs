0917699e704205d1386d87943b398940c5ff804e
Kristoffer Andersson <kod.kristoff@gmail.com>
2023-03-26 11:30:28 +0000

    refactor: dont use thiserror

--- ancestors-realm/src/app/error.rs
+++ ancestors-realm/src/app/error.rs
@@ -1,7 +1,8 @@
-use thiserror::Error;
+use crate::ui::UiError;
+use std::{error::Error as StdError, fmt::Display};
 
 /// App error
-#[derive(Debug, Error)]
+#[derive(Debug)]
 pub enum Error {
     // #[error("audio error: {0}")]
     // Audio(AudioError),
@@ -9,6 +10,23 @@ pub enum Error {
     // SaveGame(SavedGameError),
     // #[error("ui error: {0}")]
     // Ui(UiError),
-    #[error("unknown: {0}")]
     Unknown(String),
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Ui(_) => write!(f, "ui error"),
+            Self::Unknown(msg) => write!(f, "unknown: {}", msg),
+        }
+    }
+}
+
+impl StdError for Error {
+    fn source(&self) -> Option<&(dyn StdError + 'static)> {
+        match self {
+            Self::Ui(err) => Some(err),
+            Self::Unknown(_) => None,
+        }
+    }
 }
