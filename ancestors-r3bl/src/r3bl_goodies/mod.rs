d498700ead5250c36d8f176f4b5ea6c37c54ed38
Kristoffer Andersson <kod.kristoff@gmail.com>
2024-02-06 17:31:07 +0100

    refactor: move sketches

--- ancestors-r3bl/src/r3bl_goodies/mod.rs
+++
@@ -1,6 +0,0 @@
-mod color_text;
-mod tty;
-
-pub use self::color_text::*;
-pub use self::color_text_default_styles::*;
-pub use self::tty::*;
--- ancestors-realm/src/ui/components/common/mod.rs
+++
@@ -1,5 +0,0 @@
-mod areas;
-mod button;
-
-pub use self::areas::draw_area_in;
-pub use self::button::Button;
